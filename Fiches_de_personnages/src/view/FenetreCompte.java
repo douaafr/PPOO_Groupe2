0bf27b5d9af79e6a787aa4ae2ba2702839105451
Alyne LOPES DE CARVALHO <alynelopes298@gmail.com>
2024-03-19 10:59:10 +0100

    interface compte

---
+++ Fiches_de_personnages/src/view/FenetreCompte.java
@@ -0,0 +1,15 @@
+package view;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class FenetreCompte extends JFrame {
+	
+	JButton connexion = new JButton("se connecter");
+	JButton creationCompte = new JButton("créer un compte");
+	
+
+	public FenetreCompte() {
+		
+	}
+}
