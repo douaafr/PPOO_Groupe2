20d87dd27490af3e638cde2fb50bf0ef0ee93591
Alyne LOPES DE CARVALHO <alynelopes298@gmail.com>
2024-03-29 16:12:52 +0100

    test

---
+++ Fiches_de_personnages/src/model/Test.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Test {
+
+	public static void main(String args[]) {
+		Utilisateur user1 = new Utilisateur();
+		user1.setId("0001");
+		user1.setPwd("motdepasse");
+		
+		
+		
+	}
+}
