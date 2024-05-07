2f77362038faf5e3a37c0347d06306e34a7f6b7f
douaafr <douaafr19@gmail.com>
2024-05-07 11:24:29 +0200

    modif mdp

---
+++ Fiches_de_personnages/src/controller/ControllerPassword.java
@@ -0,0 +1,85 @@
+package controller;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+
+import model.Compte;
+import model.Utilisateur;
+import view.MenuAccueilView;
+import view.FenetrePassword;
+import view.MenuAccueilView;
+
+public class ControllerPassword implements ActionListener {
+
+	FenetrePassword fc;
+	Compte c = new Compte();
+	Utilisateur us = new Utilisateur();
+	JPasswordField oldPwd;
+	JPasswordField newPwd;
+	JPanel panel;
+	
+	public ControllerPassword(FenetrePassword f, JPasswordField id, JPasswordField pwd, JPanel p) {
+		this.fc = f;
+		this.oldPwd = id;
+		this.newPwd = pwd;
+		this.panel = p;
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if(((JButton)e.getSource()).getText().equals("Confirmer")) 
+		{
+			fc.user = us; 
+			char[] ancienMdpChars = oldPwd.getPassword(); 
+			char[] nvMdpChars = newPwd.getPassword();
+			String ancienMotDePasse = new String (ancienMdpChars);
+			String nvMotDePasse = new String(nvMdpChars);
+			
+			Arrays.fill(ancienMdpChars, '0'); // Pour effacer le tableau de caractère
+			Arrays.fill(nvMdpChars, '0'); // Pour effacer le tableau de caractère
+			Utilisateur user = null;
+			try {
+				us.changePwd(ancienMotDePasse, nvMotDePasse);
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			if (user == null) {
+				// Afficher id ou mpd
+				panel.setVisible(true);
+				// Au bout de temps efface la notif et ce qui a été saisie par l'utilisateur
+				int temps = 3000;
+				Timer timer = new Timer(temps, new ActionListener() {
+		            @Override
+		            public void actionPerformed(ActionEvent e) {
+		                // Masquer le JPanel
+		                panel.setVisible(false);
+
+		                // Effacer le contenu des champs de texte
+		                oldPwd.setText("");
+		                newPwd.setText("");
+
+		                // Arrêter le timer après expiration
+		                ((Timer) e.getSource()).stop();
+		            }
+		        });
+		        timer.setRepeats(false); // Ne se répète pas
+		        timer.start();
+			} else {
+				MenuAccueilView mav = new MenuAccueilView(user.getId());
+				mav.pack();
+				mav.setSize(800, 800);
+				mav.setLocationRelativeTo(null);
+				mav.setResizable(false);
+				mav.setVisible(true);
+			}
+			
+			
+		}
+	}
+}
+		
+
+
---
+++ Fiches_de_personnages/src/view/FenetrePassword.java
@@ -0,0 +1,129 @@
+package view;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import model.Compte;
+import model.Utilisateur;
+
+public class FenetrePassword extends JFrame{
+	public Compte cpt;
+	public Utilisateur user;
+	BufferedImage backgroundImage;
+	JLabel bienvenueLabel = new JLabel("Informations compte");
+	JPasswordField oldPwdField = new JPasswordField(20);
+	JPasswordField newPwdField = new JPasswordField(20);
+	JLabel oldPwdLabel = new JLabel("Mot de passe:");
+	JLabel newPwdLabel = new JLabel("Nouveau mot de passe:");
+	JButton confirmBoutton = new JButton("Confirmer");
+	JPanel panelInfo = new JPanel();
+
+	public FenetrePassword() {
+		// Initialisation de la fenêtre
+		this.setTitle("Informations compte");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // Charger l'image de fond
+        try {
+            // Charger l'image à partir d'un fichier
+        	backgroundImage = ImageIO.read(new File("backgrounds/Menu.jpg"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        // Création d'un panneau avec une image de fond
+        JPanel contentPane = new JPanel() {
+			@Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                // Dessiner l'image en tant que fond et redimensionner si nécessaire
+                if (backgroundImage != null) {
+                    g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(),  this);
+                }
+            }
+        };
+        
+        // Définir la couleur et le style d'écriture pour le thème médiéval
+        Color medievalTextColor = new Color(255, 204, 153); // Couleur beige/marron clair
+        Font medievalFont = new Font("Times New Roman", Font.BOLD, 20); // Style d'écriture médiéval
+        
+        // Personnalisation du label bienvenue
+        bienvenueLabel.setForeground(medievalTextColor);
+        bienvenueLabel.setFont(new Font("Times New Roman", Font.BOLD, 40));
+
+        // Personnalisation des champs d'identifiant et de mot de passe
+        oldPwdField.setFont(new Font("Garamond", Font.PLAIN, 16));
+        newPwdField.setFont(new Font("Garamond", Font.PLAIN, 16));
+
+        // Personnalisation des libellés pour les champs avec le style médiéval
+        oldPwdLabel.setForeground(medievalTextColor);
+        oldPwdLabel.setFont(medievalFont);
+
+        newPwdLabel.setForeground(medievalTextColor);
+        newPwdLabel.setFont(medievalFont);
+        
+        // Personnalisation des boutons de connexion et de création de compte
+        confirmBoutton.setBackground(new Color(101, 67, 33));
+        confirmBoutton.setFont(medievalFont);
+        confirmBoutton.setForeground(medievalTextColor);
+    
+        // Définition du layout null
+        contentPane.setLayout(null);
+        
+        // Ajout des éléments à l'interface utilisateur
+        bienvenueLabel.setBounds(298, 298, 180, 82); // Position et taille de JLabel "Bienvenue"
+        contentPane.add(bienvenueLabel);
+        
+        //oldPwdLabel.setForeground(new Color(139, 69, 19));
+        oldPwdLabel.setBounds(284, 420, 111, 30); // Position et taille de JLabel "Identifiant"
+        contentPane.add(oldPwdLabel);
+        
+        oldPwdField.setBounds(284, 461, 194, 30); // Position et taille de JTextField pour l'identifiant
+        contentPane.add(oldPwdField);
+        
+        //newPwdLabel.setForeground(new Color(139, 69, 19));
+        newPwdLabel.setBounds(284, 514, 127, 20); // Position et taille de JLabel "Mot de passe"
+        contentPane.add(newPwdLabel);
+        
+        newPwdField.setBounds(284, 545, 194, 30); // Position et taille de JTextField pour le mot de passe
+        contentPane.add(newPwdField);
+        
+        confirmBoutton.setBounds(199, 650, 150, 30); // Position et taille du bouton de connexion
+        contentPane.add(confirmBoutton);
+        
+        
+        // Définir le panneau avec image de fond comme contenu de la fenêtre
+        setContentPane(contentPane);
+        
+        // A afficher si l'id ou le mdp incorrect
+        panelInfo.setOpaque(false);
+        panelInfo.setBounds(216, 586, 327, 38);
+        panelInfo.setVisible(false); // Masquer le panel au début
+        contentPane.add(panelInfo);
+        
+        JLabel lblNewLabel = new JLabel("Utilisateur ou mot de passe incorrect");
+        panelInfo.add(lblNewLabel);
+        lblNewLabel.setForeground(new Color(255, 69, 0));
+        lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 18));
+        
+        //creation d'une instance de gestion d'evenement
+       // ControllerConnexion controller = new ControllerConnexion(this, oldPwdField, newPwdField, panelInfo);
+        
+        //association avec les boutons
+      	//confirmBoutton.addActionListener(controller);
+    }
+	
+
+
+}
