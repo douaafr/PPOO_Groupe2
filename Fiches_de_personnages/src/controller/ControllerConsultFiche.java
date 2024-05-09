7db03d96f9921ea477f94002732b5087c16e0d16
Alyne LOPES DE CARVALHO <alynelopes298@gmail.com>
2024-05-09 16:31:59 +0200

    consultation de fiches

---
+++ Fiches_de_personnages/src/controller/ControllerConsultFiche.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ControllerConsultFiche {
+	
+	public ControllerConsultFiche() {
+		
+	}
+
+}
---
+++ Fiches_de_personnages/src/view/FenetreConsultFiche.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class FenetreConsultFiche extends JFrame {
+	
+	public FenetreConsultFiche() {
+		
+	}
+
+}
